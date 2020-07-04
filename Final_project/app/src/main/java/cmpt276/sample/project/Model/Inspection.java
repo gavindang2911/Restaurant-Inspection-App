f173694852963cbac9b91eebc6c9ec152f1c8182
gavindang2911 <hoaidang2999@gmail.com>
2020-07-04 15:51:50 -0700

    Created inspection2,java

--- Final_project/app/src/main/java/cmpt276/sample/project/Model/Inspection.java
+++ Final_project/app/src/main/java/cmpt276/sample/project/Model/Inspection.java
@@ -1,37 +1,4 @@
-<<<<<<< Final_project/app/src/main/java/cmpt276/sample/project/Model/Inspection2.java
-package cmpt276.sample.project.Model;
 
-public class Inspection {
-    private int iconNature;
-    private String description;
-    private String Violation;
-    private int iconViolation;
-
-    public Inspection(int iconNature, String description, String Violation, int iconViolation){
-        super();
-        this.iconNature = iconNature;
-        this.description = description;
-        this.Violation = Violation;
-        this.iconViolation = iconViolation;
-    }
-
-    public int getIconNature() {
-        return iconNature;
-    }
-
-    public int getIconViolation() {
-        return iconViolation;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getViolation() {
-        return Violation;
-    }
-}
-=======
 package cmpt276.sample.project.Model;
 
 public class Inspection {
@@ -122,4 +89,3 @@ public class Inspection {
                 '}';
     }
 }
->>>>>>> Final_project/app/src/main/java/cmpt276/sample/project/Model/Inspection.java
