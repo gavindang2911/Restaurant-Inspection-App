6b663c912a6aa0a9683ae9abd665a3aa5ce8b9a5
Luxi <lxwang@sfu.ca>
2020-08-03 00:14:06 -0700

    Add button

---
+++ Final_project/app/src/main/java/cmpt276/sample/project/UI/SearchActivity.java
@@ -0,0 +1,16 @@
+package cmpt276.sample.project.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cmpt276.sample.project.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
